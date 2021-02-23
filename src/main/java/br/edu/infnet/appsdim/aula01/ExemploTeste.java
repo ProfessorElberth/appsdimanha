a07f45d13ab369651253b5a7757128f814fdbfe7
elberth moraes <elberth.moraes@gmail.com>
2021-02-23 08:42:49 -0300

    Criacao do projeto e estrutura de classes

---
+++ src/main/java/br/edu/infnet/appsdim/aula01/ExemploTeste.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.appsdim.aula01;
+
+public class ExemploTeste {
+
+	public static void main(String[] args) {
+
+		Aluno aluno = new Aluno();
+		aluno.setId(1);
+		aluno.setNome("Elberth Moraes");
+		aluno.setEmail("elberth.moraes@prof.infnet.edu.br");
+		System.out.println(aluno);
+	}
+}
